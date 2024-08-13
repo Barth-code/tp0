54fdfd90827e0c363621f7e508bf7d3183c6beae
Barth-code <bartaydev@gmail.com>
2024-08-13 16:30:07 -0400

    cook log

--- src/components/Cooks.jsx
+++ src/components/Cooks.jsx
@@ -1,8 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 const Cooks = () => {
-  useEffect(() => {}, []);
+  const [cook, useCook] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s")
+      .then((res) => console.log(res.data.meals));
+  }, []);
   return (
     <ul className="cook">
       <Card />
