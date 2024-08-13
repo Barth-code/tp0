e11a78b42c469ce8ef4574b8e9f6b8ef5c4b2b84
Barth-code <bartaydev@gmail.com>
2024-08-13 14:36:50 -0400

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Card = () => {
-  return <li className="card-container">Salut</li>;
+  return (
+    <li className="card-container">
+      <h1>Nom aliment</h1>
+      <p>petit description</p>
+      <img src="" alt="aliment" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem culpa
+        quidem dolor recusandae quod, ipsum corrupti nulla commodi aperiam, amet
+        assumenda eveniet nemo odit suscipit ratione harum exercitationem atque
+        voluptate.
+      </p>
+    </li>
+  );
 };
 
 export default Card;
